a04cc398ebfe2a8f4cada4e6f2bad5d0d3537408
Youness <taykileo@gmail.com>
2025-08-01 21:01:38 -0400

    Update code so that tables show all columns content

--- o2o/demo.py
+++ o2o/demo.py
@@ -1,5 +1,7 @@
 def main():
 
+    import pandas as pd
+    pd.set_option('display.max_columns', None)
     from o2o import BivariateHawkesProcessSimulator, ParameterEstimator, O2OAnalyzer
 
     M = int(input('Give the number of users:   '))
