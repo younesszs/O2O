8d1e141c908c80e81c655c6f1d133cfdcd70e754
Youness <taykileo@gmail.com>
2025-08-04 23:05:08 -0400

    create new token

--- o2o/demo.py
+++ o2o/demo.py
@@ -1,7 +1,6 @@
 def main():
 
     import pandas as pd
-    pd.set_option('display.max_columns', None)
     from o2o import BivariateHawkesProcessSimulator, ParameterEstimator, O2OAnalyzer
 
     M = int(input('Give the number of users:   '))
